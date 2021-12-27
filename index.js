206ede3aa877c86d3e486a3490873a43718b1c06
ChintzRuparel <chintz0952@gmail.com>
2021-12-27 12:09:00 +0530

    finalbuild

---
+++ index.js
@@ -0,0 +1,47 @@
+const billAmount = document.querySelector("#bill-amount");
+
+const cashGiven = document.querySelector("#cash-given");
+
+const checkButton = document.querySelector("#check-button");
+
+const message = document.querySelector("#error-message");
+
+const noOfNotes = document.querySelectorAll(".no-of-notes");
+
+const availableNotes = [2000, 500, 100, 50, 20, 10, 5, 1];
+
+checkButton.addEventListener("click", function validateBillAndCashAmount() {
+  hideMessage();
+  if (billAmount.value > 0) {
+
+    if (cashGiven.value >= billAmount.value) {
+      
+        const amountToBeReturned = cashGiven.value - billAmount.value; 
+      
+        calculateChange(amountToBeReturned);
+    } else {
+      
+        showMessage("U broke");
+    }
+  } else {
+    
+    showMessage("Error!");
+  }
+});
+
+function calculateChange(amountToBeReturned) {
+  for (let i = 0; i < availableNotes.length; i++) {
+        const numberOfNotes = Math.trunc(amountToBeReturned / availableNotes[i]);
+        amountToBeReturned = amountToBeReturned % availableNotes[i];
+        noOfNotes[i].innerText = numberOfNotes;
+  }
+}
+
+function hideMessage() {
+  message.style.display = "none";
+}
+
+function showMessage(msg) {
+  message.style.display = "block";
+  message.innerText = msg;
+}
