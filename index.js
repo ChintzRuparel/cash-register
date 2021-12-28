ec13e53303cba72e9d79a21f3ad94edbee778b0b
ChintzRuparel <chintz0952@gmail.com>
2021-12-28 10:16:01 +0530

    ui build changes

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const message = document.querySelector("#error-message");
 
 const noOfNotes = document.querySelectorAll(".no-of-notes");
 
-const availableNotes = [2000, 500, 100, 50, 20, 10, 5, 1];
+const availableNotes = [2000, 500, 100,20, 10, 5, 1];
 
 checkButton.addEventListener("click", function validateBillAndCashAmount() {
   hideMessage();
